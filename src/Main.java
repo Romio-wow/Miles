72ef88848d49257514173a0c8f149f0b2d8c1bea
Roman Kravchenko <roman.me@mail.ru>
2020-05-01 13:51:53 +0300

    fix

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
        int ticketPrice = 8000;
        int coins = 20 ;
-       int bonusmiles = ticketPrice / coins;
-       System.out.println(bonusmiles);
+       int bonusMiles = ticketPrice / coins;
+       System.out.println(bonusMiles);
 
 
     }
