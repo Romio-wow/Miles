db3579ca7c3169a5f7c2f11cf49f70725edac173
Roman Kravchenko <roman.me@mail.ru>
2020-05-01 10:02:42 +0300

    bonus

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+       int ticketprice = 8000;
+       int rubles = 20 ;
+       int bonusmiles = ticketprice / rubles;
+        System.out.println(bonusmiles);
+
+
+    }
+}
